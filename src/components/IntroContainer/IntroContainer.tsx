4eaa592a721fb5cdc33069d88aa64751ed0e678b
Phạm Vĩnh Tài <kainesv86@gmail.com>
2023-03-09 13:06:38 +0700

    Fix color

--- src/components/IntroContainer/IntroContainer.tsx
+++ src/components/IntroContainer/IntroContainer.tsx
@@ -100,7 +100,6 @@ const IntroContainer = (props: IntroContainerProps) => {
 
   return (
     <div className={classes.background}>
-      {/* <TwilioLogo className={classes.twilioLogo} /> */}
       {user && location.pathname !== '/login' && <UserMenu />}
       <div className={classes.container}>
         <div className={classes.innerContainer}>
